0b88bb633378837e1add167194df168a7a7e3454
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-03-31 16:27:28 +0900

    chore: @RestControllerAdvice 변경 "org.iimsa"

--- src/main/java/org/iimsa/common/response/CommonResponseAdvice.java
+++ src/main/java/org/iimsa/common/response/CommonResponseAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@RestControllerAdvice(basePackages = "org.spartahub")
+@RestControllerAdvice(basePackages = "org.iimsa")
 public class CommonResponseAdvice implements ResponseBodyAdvice<Object>  {
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
