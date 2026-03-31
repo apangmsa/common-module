ced6482e3f3b3068c8cd7e4b56899d12ccb39a5a
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-03-31 16:27:28 +0900

    chore: @RestControllerAdvice 변경 "org.iimsa"

--- src/main/java/org/iimsa/common/exception/GlobalExceptionAdviceImpl.java
+++ src/main/java/org/iimsa/common/exception/GlobalExceptionAdviceImpl.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice("org.spartahub")
+@RestControllerAdvice("org.iimsa")
 public class GlobalExceptionAdviceImpl implements GlobalExceptionAdvice {
 
     // 직접 정의한 CustomException 처리
